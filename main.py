1b4ee851eeeb7364acdeb1ddcbc2dd35e210798a
Alex Testa <alex.testa@studio.unibo.it>
2022-01-26 16:20:27 +0100

    introduce color

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ name_file = str(input("[*]Please, digit the name of file you want create ? : "))
 text = str(input("[*]Please, digit the text of file you want create ? : "))
 name_file = name_file + '.png'
 
-kit.text_to_handwriting(text, save_to = name_file )
+kit.text_to_handwriting(text,name_file, [0,0,138])
 img = cv2.imread(name_file)
 cv2.imshow("Converted Text", img)
 cv2.waitKey(0)
