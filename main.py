990fa869441bb37d502f6155e89ef68813b1fc27
Alex Testa <alex.testa@studio.unibo.it>
2022-01-26 16:15:42 +0100

    start program

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Alex Testa
+"""
+
+import pywhatkit as kit
+import cv2
+
+name_file = str(input("[*]Please, digit the name of file you want create ? : "))
+text = str(input("[*]Please, digit the text of file you want create ? : "))
+name_file = name_file + '.png'
+
+kit.text_to_handwriting(text, save_to = name_file )
+img = cv2.imread(name_file)
+cv2.imshow("Converted Text", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
